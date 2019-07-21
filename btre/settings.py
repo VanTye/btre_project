bf6b494072201ebb489beaa062751d23f2c5a41d
WernCheen Tye <s3647030@student.rmit.edu.au>
2019-07-21 17:11:42 +0800

    Edit Secret Key

--- btre/settings.py
+++ btre/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aei%8x^+7)42@u#((7=#=8%k6mv2_jgh6)y+qoh--=2gj98y5v'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['167.71.206.214']
 
 
 # Application definition
